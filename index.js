ff5e13bbafbd1f5319f5ea7916cff2e0311a96a0
shkr <shashank.f1@gmail.com>
2021-09-15 15:51:17 +0530

    fix

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const core = require('elementary-core');
+
 const el = require('@nick-thompson/elementary');
+const core = global.elementary.core;
 const reload = require('livereload')
 
-// This example is the "Hello, world!" of writing audio processes in Elementary.
-//
-// To start, we await the "load" event, which is the point at which the Elementary
-// runtime is prepared to start receiving render instructions. Then, we make a single
-// call to `core.render` with two arguments. Each argument here is an instruction for
-// what to render in the respective output channel, so with a stereo output pair, the first
-// argument will render to the left channel, and the second argument will render to the right.
-//
-// The signal we're generating here is a simple sine tone via `el.cycle` at 440Hz in the left
-// channel and 441Hz in the right, creating some interesting binaural beating. Each sine tone is
-// then multiplied by 0.3 to apply some simple gain before going to the output. That's it!
+function saw(frequency) {
+  return el.sub(el.phasor(frequency), 0.5);
+}
+
+
 core.on('load', function() {
   core.render(
-    el.mul(0.3, el.cycle(40)),
-    el.mul(0.3, el.cycle(0141)),
+    saw(240)
   );
 });
 
